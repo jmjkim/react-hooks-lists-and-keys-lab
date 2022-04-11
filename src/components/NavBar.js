382802159fc3ce7400f4b3c09f77a5d6015b9b42
Jongmin Jack Kim <jmjkim00@icloud.com>
2022-04-11 15:39:51 -0700

    Completed assignment

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,8 +2,11 @@ import React from "react";
 
 function NavBar() {
   const links = ["home", "about", "projects"];
+  const navItems = links.map((link) => {
+    return <a href={`#${link}`} key={link}>{link}</a>
+  })
 
-  return <nav>{/* display an <a> tag for each link here */}</nav>;
+  return <nav>{navItems}</nav>;
 }
 
 export default NavBar;
